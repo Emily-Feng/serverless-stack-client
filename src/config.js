fabf72e55f370ab746b6090a936e8e4ab69b507b
Emily-Feng <60203331+Emily-Feng@users.noreply.github.com>
2021-05-28 15:36:04 -0400

    Update config.js
    config typo

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ const config = {
       URL: "https://9qe1re22yb.execute-api.us-east-1.amazonaws.com/prod",
     },
     cognito: {
-      REGION: "us-east-q",
+      REGION: "us-east-1",
       USER_POOL_ID: "us-east-1_0Xim1EefX",
       APP_CLIENT_ID: "11frmc450tlrkgj0fahsrhnsej",
       IDENTITY_POOL_ID: "us-east-1:72b558e6-09e8-499c-81c8-ca8638337dec",
